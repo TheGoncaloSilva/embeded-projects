084eecae4a75ef46a9726592a0ebf2caa5914e32
Goncalo Silva <goncalo.lslv.silva@gmail.com>
2022-12-25 17:03:04 +0000

    Further changes and improvements to remote_display

--- esp32/remote_display/main/remote_display.c
+++ esp32/remote_display/main/remote_display.c
@@ -1,9 +1,16 @@
 // code from https://github.com/lowlevellearning/esp32-wifi/blob/main/main/wifi-connection.c
 #include <string.h>
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/i2c.h"
 #include "esp_system.h"
+#include "esp_err.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -14,7 +21,8 @@
 #include "lwip/sys.h"
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
-#include "i2c_oled_example_main.c"
+
+#include "lvgl.h"
 
 /** DEFINES **/
 #define WIFI_SUCCESS 1 << 0
@@ -28,6 +36,24 @@
 #define FALSE 0
 #define TRUE 1
 
+#define I2C_HOST  0
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ    (400 * 1000)
+#define EXAMPLE_PIN_NUM_RST           -1
+#define EXAMPLE_I2C_HW_ADDR           0x3C
+
+// The pixel number in horizontal and vertical
+#define EXAMPLE_LCD_H_RES              128
+#define EXAMPLE_LCD_V_RES              64
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+#define EXAMPLE_LVGL_TICK_PERIOD_MS    2
+
 /** GLOBALS **/
 
 // event group to contain status information
@@ -38,10 +64,11 @@ static int s_retry_num = 0;
 
 // task tag
 static const char *TAG = "WIFI";
+static const char *TAG2 = "I2C";
 /** FUNCTIONS **/
 
 /* Start I2C*/
-extern lv_disp_t *i2c_main();
+extern void example_lvgl_demo_ui(lv_disp_t *disp);
 /* End I2C */
 
 //event handler for wifi events
@@ -197,15 +224,142 @@ int handle_request(char *request){
     return TRUE;
 }
 
-void example_lvgl_demo_ui(lv_disp_t *disp)
+/* START I2C interaction */
+//---------------------------------------- //
+// +++++++++++++++++++++++++++++++++++++++ //
+
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
 {
-    lv_obj_t *scr = lv_disp_get_scr_act(disp);
-    lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
-    lv_obj_set_width(label, 128);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+    lv_disp_drv_t *disp_driver = (lv_disp_drv_t *)user_ctx;
+    lv_disp_flush_ready(disp_driver);
+    return false;
 }
+
+static void example_lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
+{
+    esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t) drv->user_data;
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+    // copy a buffer's content to a specific area of the display
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, color_map);
+}
+
+static void example_lvgl_set_px_cb(lv_disp_drv_t *disp_drv, uint8_t *buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+                                   lv_color_t color, lv_opa_t opa)
+{
+    uint16_t byte_index = x + (( y >> 3 ) * buf_w);
+    uint8_t  bit_index  = y & 0x7;
+
+    if ((color.full == 0) && (LV_OPA_TRANSP != opa)) {
+        buf[byte_index] |= (1 << bit_index);
+    } else {
+        buf[byte_index] &= ~(1 << bit_index);
+    }
+}
+
+static void example_lvgl_rounder(lv_disp_drv_t *disp_drv, lv_area_t *area)
+{
+    area->y1 = area->y1 & (~0x7);
+    area->y2 = area->y2 | 0x7;
+}
+
+static void example_increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
+}
+
+lv_disp_t* i2c_main()
+{
+    static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+    static lv_disp_drv_t disp_drv;      // contains callback functions
+
+    ESP_LOGI(TAG2, "Initialize I2C bus");
+    i2c_config_t i2c_conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = GPIO_NUM_21, // My specific board data pin
+        .scl_io_num = GPIO_NUM_22, // My specific board clock pin
+        .sda_pullup_en = GPIO_PULLUP_ENABLE, // Use the internal pullup resistor
+        .scl_pullup_en = GPIO_PULLUP_ENABLE, // Use the internal pullup resistor
+        .master.clk_speed = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+    };
+    ESP_ERROR_CHECK(i2c_param_config(I2C_HOST, &i2c_conf));
+    ESP_ERROR_CHECK(i2c_driver_install(I2C_HOST, I2C_MODE_MASTER, 0, 0, 0));
+
+    ESP_LOGI(TAG2, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t io_config = {
+        .dev_addr = EXAMPLE_I2C_HW_ADDR,
+        .control_phase_bytes = 1,               // According to SSD1306 datasheet
+        .dc_bit_offset = 6,                     // According to SSD1306 datasheet
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,   // According to SSD1306 datasheet
+        .lcd_param_bits = EXAMPLE_LCD_CMD_BITS, // According to SSD1306 datasheet
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+        .user_ctx = &disp_drv,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)I2C_HOST, &io_config, &io_handle));
+
+    ESP_LOGI(TAG2, "Install SSD1306 panel driver");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .bits_per_pixel = 1,
+        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    ESP_LOGI(TAG2, "Initialize LVGL library");
+    lv_init();
+    // alloc draw buffers used by LVGL
+    // it's recommended to choose the size of the draw buffer(s) to be at least 1/10 screen sized
+    lv_color_t *buf1 = malloc(EXAMPLE_LCD_H_RES * 20 * sizeof(lv_color_t));
+    assert(buf1);
+    lv_color_t *buf2 = malloc(EXAMPLE_LCD_H_RES * 20 * sizeof(lv_color_t));
+    assert(buf2);
+    // initialize LVGL draw buffers
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, EXAMPLE_LCD_H_RES * 20);
+
+    ESP_LOGI(TAG2, "Register display driver to LVGL");
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = EXAMPLE_LCD_H_RES;
+    disp_drv.ver_res = EXAMPLE_LCD_V_RES;
+    disp_drv.flush_cb = example_lvgl_flush_cb;
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.user_data = panel_handle;
+    disp_drv.rounder_cb = example_lvgl_rounder;
+    disp_drv.set_px_cb = example_lvgl_set_px_cb;
+    lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+
+    ESP_LOGI(TAG2, "Install LVGL tick timer");
+    // Tick interface for LVGL (using esp_timer to generate 2ms periodic event)
+    const esp_timer_create_args_t lvgl_tick_timer_args = {
+        .callback = &example_increase_lvgl_tick,
+        .name = "lvgl_tick"
+    };
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, EXAMPLE_LVGL_TICK_PERIOD_MS * 1000));
+    
+    ESP_LOGI(TAG, "Display LVGL Scroll Text");
+    example_lvgl_demo_ui(disp);
+
+    while (1) {
+        // raise the task priority of LVGL and/or reduce the handler period can improve the performance
+        vTaskDelay(pdMS_TO_TICKS(10));
+        // The task running lv_timer_handler should have lower priority than that running `lv_tick_inc`
+        lv_timer_handler();
+    }
+    return disp;
+}
+
+//---------------------------------------- //
+// +++++++++++++++++++++++++++++++++++++++ //
+/* End I2C interaction */
 
 // connect to the server and return the result
 esp_err_t connect_tcp_server(void)
@@ -262,7 +416,7 @@ esp_err_t connect_tcp_server(void)
                 vTaskDelay(pdMS_TO_TICKS(10));
                 // The task running lv_timer_handler should have lower priority than that running `lv_tick_inc`
                 lv_timer_handler();
-        }
+            }
         }else
             response = failureResponse; // Response points to failure
 
